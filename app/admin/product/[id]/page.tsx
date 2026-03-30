b05cc6c0ef08c635a29bedf03cde9225addc58a8
LeninMiranda <lenin9073@gmail.com>
2026-03-29 19:54:50 -0700

    productContextType

--- app/admin/product/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import AdminNavBar from "@/app/components/NavBar/AdminNavBar";
-import { useState } from "react";
-
-export default function AdminProductPage() {
-  const [showAddModal, setShowAddModal] = useState(false);
-}
--- app/interfaces/products.ts
+++ app/interfaces/products.ts
@@ -2,37 +2,14 @@ import { StaticImageData } from "next/image";
 
 export interface Product {
   id: number;
-  name: string;
-  slug: string;
   description: string;
   price: number;
   rating: number;
   category: string;
   inStock: number;
-  image: string | undefined;
-  main_image?: string;
-  status: string;
-  isActive: boolean;
-  isFeatured: boolean;
+  image: string | StaticImageData | undefined;
+  name: string;
   quantity?: number;
-  created_at?: string;
-  updated_at?: string;
-  images?: Array<{ id: number; image: string; alt_text: string }>;
-}
-
-export interface ProductContextType {
-  products: Product[];
-  setProducts: React.Dispatch<React.SetStateAction<Product[]>>;
-  fetchProducts: () => Promise<void>;
-  fetchProductsBySlug: (slug: string) => Promise<Product>;
-  createNewProduct: (productData: FormData) => Promise<void>;
-  updateExistingProduct: (id: number, productData: FormData) => Promise<void>;
-  deleteExistingProduct: (id: number) => Promise<void>;
-  showAddModal: boolean;
-  setShowAddModal: React.Dispatch<React.SetStateAction<boolean>>;
-  showDeleteModal: boolean;
-  setShowDeleteModal: React.Dispatch<React.SetStateAction<boolean>>;
-  isLoading: boolean;
 }
 
 export interface SearchBarProps {
@@ -40,24 +17,17 @@ export interface SearchBarProps {
   onSearchChange: (term: string) => void;
 }
 
-export interface EditProductsModalProps {
-  product: Product | null;
-  onClose: () => void;
-  onSave: (updatedProduct: Product) => void;
-}
-
-export interface ProductCardProps {
-  product: Product;
-  onEdit: (product: Product) => void;
-  onDelete: (id: number) => void;
-}
-
-export interface ProductFormData {
-  name: string;
-  slug: string;
-  price: number;
-  description: string;
-  inStock: number;
-  mainImage: File | null;
-  isFeatured: boolean;
+export interface ProductContextType {
+  products: Product[];
+  setProducts: React.Dispatch<React.SetStateAction<Product[]>>;
+  fetchProducts: () => Promise<void>;
+  fetchProductsBySlug: (slug: string) => Promise<void>;
+  createNewProduct: (productData: FormData) => Promise<void>;
+  updateExistingProduct: (id: number, productData: FormData) => Promise<void>;
+  deleteExistingProduct: (id: number) => Promise<void>;
+  isLoading: boolean;
+  showAddModal: boolean;
+  setShowAddModal: React.Dispatch<React.SetStateAction<boolean>>;
+  showDeleteModal: boolean;
+  setShowDeleteModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
